0526d20099fcf24d8f25c1fe547b7d71e93f6ee9
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2023-04-30 16:07:35 +0400

    top btn

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
-const search_icon = document.querySelector(".search_icon");
-const search_input = document.querySelector(".search_input");
-const searchbar_container = document.querySelector(".searchbar_container");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Hello World!");
 
-const header_logo_dine = document.querySelector(".header_logo_dine");
-const header_text = document.querySelector(".header_text");
 
-search_icon.addEventListener("click",function(){
-    header_logo_dine.classList.toggle("hide_item");
-    header_text.classList.toggle("hide_item");
-    search_input.classList.toggle("hide_item");
-})
+    const search_icon = document.querySelector(".search_icon");
+    const search_input = document.querySelector(".search_input");
+    const searchbar_container = document.querySelector(".searchbar_container");
+    
+    const top_button = document.querySelector(".up_icon_small")
+    
+    const header_logo_dine = document.querySelector(".header_logo_dine");
+    const header_text = document.querySelector(".header_text");
+    
+    if(search_icon){
+        search_icon.addEventListener("click",function(){
+            header_logo_dine.classList.toggle("hide_item");
+            header_text.classList.toggle("hide_item");
+            search_input.classList.toggle("hide_item");
+        })
+    }
+
+    
+    // function topFunction() {
+    //     console.log("top");
+    //     document.body.scrollTop = 0;
+    //     document.documentElement.scrollTop = 0;
+    // }
+    
+    top_button.addEventListener("click", function() {
+        console.log("top");
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
+
+  });
